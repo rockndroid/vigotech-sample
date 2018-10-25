294c137185526bca1df2471fab4036fc5006bad1
saulmm <saulmm2@gmail.com>
2018-10-25 18:22:44 +0200

    Enabled transitions for glide

--- app/src/main/java/com/example/vigotecth/ui/groups_list/GroupViewHolder.kt
+++ app/src/main/java/com/example/vigotecth/ui/groups_list/GroupViewHolder.kt
@@ -1,9 +1,12 @@
 package com.example.vigotecth.ui.groups_list
 
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import com.example.vigotecth.data.model.Group
 import com.example.vigotecth.databinding.ListitemGroupBinding
 
@@ -15,10 +18,13 @@ class GroupViewHolder(private val binding: ListitemGroupBinding):
 
         Glide.with(itemView.context)
             .load(group.image)
+            .transition(DrawableTransitionOptions.withCrossFade())
             .into(binding.imgGroup)
 
         binding.root.setOnClickListener {
-            itemView.context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(group.url)))
+            itemView.context.startActivity(
+                Intent(Intent.ACTION_VIEW, Uri.parse(group.url))
+            )
         }
     }
 }
